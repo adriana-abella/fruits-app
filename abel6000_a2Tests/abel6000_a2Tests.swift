71d2fdb03b9d3e62c086bf2152502ec4930983cf
adriana-abella <56332223+adriana-abella@users.noreply.github.com>
2020-01-29 20:06:59 -0500

    Initial Commit

---
+++ abel6000_a2Tests/abel6000_a2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  abel6000_a2Tests.swift
+//  abel6000_a2Tests
+//
+//  Created by Adriana Abella on 2020-01-29.
+//  Copyright © 2020 wlu. All rights reserved.
+//
+
+import XCTest
+@testable import abel6000_a2
+
+class abel6000_a2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
